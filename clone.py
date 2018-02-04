c7a0beecb2a1eaee22c4d1132d3bb481df2ff6e1
ly0n <ly0@protonmail.com>
2018-02-04 21:37:49 +0100

    image preprocessing

--- clone.py
+++ clone.py
@@ -24,13 +24,15 @@ X_train = np.array(images)
 y_train = np.array(steerings)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense,Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20),(0,0)),input_shape=(3,160,320))
+model.add(lambda x: x / 255.0 - 0.5))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2,shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2,shuffle=True, nb_epoch=7,verbose=1)
 
 model.save('model.h5')
