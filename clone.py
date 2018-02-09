883567607f4ca83f2d278893ec092a086f5855aa
ly0n <ly0@protonmail.com>
2018-02-09 11:43:10 +0100

    added generator

--- clone.py
+++ clone.py
@@ -3,62 +3,115 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import imageio
+import sklearn
 
+EPOCHS = 1
+cropx_start = 20
+cropx_stop = 110
+x_new = cropx_stop - cropx_start
+y_new = 320
 
-
-lines = []
+samples = []
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
+def crop_image(image):
+    cropped = image[cropx_start:cropx_stop,0:y_new]
+    return cropped
 
-images = []
-steerings = []
-for line in lines:
-    source_path = line[0]
-    filename1 = line[0].split('/')[-1]
-    filename2 = line[1].split('/')[-1]
-    filename3 = line[2].split('/')[-1]
-    current_path = './data/IMG/'
-    correction = 0.2
-    steering_center = float(line[3])
-    steering_left = steering_center + correction
-    steering_right = steering_center - correction
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-    img_center = cv2.imread(current_path + filename1)
-    img_left = cv2.imread(current_path + filename2)
-    img_right = cv2.imread(current_path + filename3)
+def generator(samples,batch_size=128):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0,num_samples,batch_size):
+                 batch_samples = samples[offset:offset+batch_size]
+                 images = []
+                 steerings = []
+                 for batch_sample in batch_samples:
+                    filename1 = batch_sample[0].split('/')[-1]
+                    filename2 = batch_sample[1].split('/')[-1]
+                    filename3 = batch_sample[2].split('/')[-1]
+                    current_path = './data/IMG/'
+                    correction = 0.2
+                    steering_center = float(batch_sample[3])
+                    steering_left = steering_center + correction
+                    steering_right = steering_center - correction
 
-    images.append(img_center)
-    images.append(img_left)
-    images.append(img_right)
-    steerings.append(steering_center)
-    steerings.append(steering_left)
-    steerings.append(steering_right)
+                    images.append(crop_image(imageio.imread(current_path + filename1)))
+                    images.append(crop_image(imageio.imread(current_path + filename2)))
+                    images.append(crop_image(imageio.imread(current_path + filename3)))
 
-# Keras needs np array datatype
-X_train = np.array(images)
-print(X_train.shape)
-y_train = np.array(steerings)
-print(y_train.shape)
+                    steerings.append(steering_center)
+                    steerings.append(steering_left)
+                    steerings.append(steering_right)
+
+                    # Keras needs np array datatype
+                    X_train = np.array(images)
+                    y_train = np.array(steerings)
+
+                    yield sklearn.utils.shuffle(X_train,y_train)
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense,Lambda,Cropping2D
+from keras.layers import Flatten, Dense,Lambda,Cropping2D,Conv2D,Dropout
 
-model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5,input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((50,20),(0,0))))
-model.add(Flatten())
-model.add(Dense(1))
+model = Sequential([
+    Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(x_new, y_new, 3)),
+    # Convolutional layer 1 24@31x98 | 5x5 kernel | 2x2 stride | elu activation
+    Conv2D(24, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal'),
+    # Dropout with drop probability of .1 (keep probability of .9)
+    Dropout(.1),
+    # Convolutional layer 2 36@14x47 | 5x5 kernel | 2x2 stride | elu activation
+    Conv2D(36, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal'),
+    # Dropout with drop probability of .2 (keep probability of .8)
+    Dropout(.2),
+    # Convolutional layer 3 48@5x22  | 5x5 kernel | 2x2 stride | elu activation
+    Conv2D(48, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal'),
+    # Dropout with drop probability of .2 (keep probability of .8)
+    Dropout(.2),
+    # Convolutional layer 4 64@3x20  | 3x3 kernel | 1x1 stride | elu activation
+    Conv2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal'),
+    # Dropout with drop probability of .2 (keep probability of .8)
+    Dropout(.2),
+    # Convolutional layer 5 64@1x18  | 3x3 kernel | 1x1 stride | elu activation
+    Conv2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal'),
+    # Flatten
+    Flatten(),
+    # Dropout with drop probability of .3 (keep probability of .7)
+    Dropout(.3),
+    # Fully-connected layer 1 | 100 neurons | elu activation
+    Dense(100, activation='elu', init='he_normal'),
+    # Dropout with drop probability of .5
+    Dropout(.5),
+    # Fully-connected layer 2 | 50 neurons | elu activation
+    Dense(50, activation='elu', init='he_normal'),
+    # Dropout with drop probability of .5
+    Dropout(.5),
+    # Fully-connected layer 3 | 10 neurons | elu activation
+    Dense(10, activation='elu', init='he_normal'),
+    # Dropout with drop probability of .5
+    Dropout(.5),        # Output
+    Dense(1, activation='linear', init='he_normal')
+])
 
+model.summary()
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2,shuffle=True, nb_epoch=7,verbose=1)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator,
+            nb_val_samples=len(validation_samples), nb_epoch=EPOCHS)
 
 
 #history_object = model.fit_generator(train_generator, samples_per_epoch =
-#    len(train_samples), validation_data = 
+#    len(train_samples), validation_data =
 #    validation_generator,
-#    nb_val_samples = len(validation_samples), 
+#    nb_val_samples = len(validation_samples),
 #    nb_epoch=5, verbose=1)
 #
 #### print the keys contained in the history object
